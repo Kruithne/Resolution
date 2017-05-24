91d53128f4a551e4fb596a25cdc988fc96bf1a0d
Kruithne <kruithne@gmail.com>
2017-05-24 15:03:06 +0100

    Add license boilerplate to Lua file

---
+++ Resolution.lua
@@ -0,0 +1,8 @@
+--[[
+	Resolution (C) Kruithne <kruithne@gmail.com>
+	Licensed under GNU General Public Licence version 3.
+	
+	https://github.com/Kruithne/Resolution
+
+	Resolution.lua - Core add-on core/functions.
+]]--
