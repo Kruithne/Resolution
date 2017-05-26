a00b85de0eef59141d5b267d0fe6e6ab93f573fd
Kruithne <kruithne@gmail.com>
2017-05-26 06:19:30 +0100

    The basic beginnings

--- Resolution.lua
+++ Resolution.lua
@@ -6,3 +6,90 @@
 
 	Resolution.lua - Core add-on core/functions.
 ]]--
+
+Resolution = {}; -- Primary add-on container.
+
+-- Local Optimization
+local _R = Resolution;
+local _K = Krutilities;
+
+-- Constants and Colors
+_R.ADDON_NAME = "Resolution";
+_R.MAIN_BACKDROP_COLOR = CreateColor(0, 0, 0, 0.85);
+_R.GENERIC_FRAME_STYLE = { tile = true, tileSize = 32, edgeSize = 1, bgFile = "TILESET\\GENERIC\\Grey", edgeFile = "TILESET\\GENERIC\\Grey", insets = { left = 2, right = 2, top = 2, bottom = 2 }};
+
+_R.Print = function(self, message)
+	DEFAULT_CHAT_FRAME:AddMessage(message);
+end
+
+_R.OnAddonLoaded = function(self, addonName)
+	if addonName == _R.ADDON_NAME then
+		self:OnLoad();
+	end
+end
+
+_R.CreateCornerButton = function(self, name, icon)
+	local spawnData = {
+		size = 48,
+		name = name,
+		textures = {
+			{
+				setAllPoints = true,
+				texture = "Interface\\EncounterJournal\\UI-ENCOUNTERJOURNALTEXTURES",
+				texCoord = {0.72265625, 0.81640625, 0.01953125, 0.06640625},
+				subLevel = 0
+			},
+			{
+				size = 26,
+				points = { point = "CENTER" },
+				texture = "Interface\\ICONS\\" .. icon,
+				subLevel = -1,
+				mask = "Interface\\CharacterFrame\\TempPortraitAlphaMask"
+			}
+		}
+	};
+
+	if self.lastCornerButton then
+		-- Anchor to the last button that was spawned.
+		spawnData.points = { point = "RIGHT", relativeTo = self.lastCornerButton, relativePoint = "LEFT", x = 5 };
+	else
+		-- Anchor to the main frame itself.
+		spawnData.points = { point = "TOPRIGHT", x = -5, y = -5 }
+	end
+
+	self.lastCornerButton = self.frameMain:SpawnFrame(spawnData);
+end
+
+_R.OnLoad = function(self)
+	self.frameMain = _K:Frame({
+		width = 900, height = 500,
+		strata = "FULLSCREEN",
+		points = {point = "CENTER"},
+		name = "ResolutionFrame",
+		backdrop = self.GENERIC_FRAME_STYLE,
+		backdropColor = self.MAIN_BACKDROP_COLOR,
+		backdropBorderColor = self.MAIN_BACKDROP_COLOR
+	});
+
+	-- These will render from right to left.
+	self:CreateCornerButton("$parentCloseButton", "Spell_Shadow_SacrificialShield");
+	self:CreateCornerButton("$parentSettingsButton", "INV_Eng_GearspringParts");
+
+	self:Print("Loaded!");
+end
+
+-- Event Handler
+_K.EventHandler(_R, {
+	["ADDON_LOADED"] = "OnAddonLoaded"
+});
+
+
+--[[local testFrame = CreateFrame("FRAME", "ResolutionFrame", UIParent);
+testFrame:SetPoint("CENTER", 0, 0);
+testFrame:SetWidth(64);
+testFrame:SetHeight(64);
+
+local testTex = testFrame:CreateTexture();
+testTex:SetTexture("Interface\\ICONS\\6BF_Retched_Blackrock");
+testTex:SetMask("Interface\\COMMON\\icon-shadow");
+testTex:SetAllPoints(testFrame);]]--
